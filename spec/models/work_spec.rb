fe3820492afda8e7de8ae466b44bf5a9156f2e1f
Nicola Bernardini <nicb@sme-ccppd.org>
2016-01-15 18:46:51 +0100

    added i/o conditioning for object work

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Work, type: :model do
 
   let(:args) {
-    { :title => 'Test', :year => '2016-01-01', :duration => '0000-01-01 00:03:03', :instruments => 'pno', :program_notes_en => 'test notes', :program_notes_it => 'note di test' }
+    { :title => 'Test', :year => Time.zone.parse('2016-01-01'), :duration => Time.zone.parse('00:03:03'), :instruments => 'pno', :program_notes_en => 'test notes', :program_notes_it => 'note di test' }
   }
 
   it 'does full field validation' do
@@ -24,6 +24,25 @@ RSpec.describe Work, type: :model do
       expect(cur_args.delete(field_to_delete)).to eq(args[field_to_delete])
       expect { Work.create!(cur_args) }.to raise_error(ActiveRecord::RecordInvalid)
     end
+  end
+
+  context 'object i/o' do
+
+    it 'conditions a year insertion appropriately' do
+      expect((w = Work.create!(args)).valid?).to eq(true)
+      expect(w.year).to eq(args[:year] + 2.days)
+    end
+  
+    it 'conditions the display of duration appropriately' do
+      expect((w = Work.create!(args)).valid?).to eq(true)
+      expect(w.display_duration).to eq(args[:duration].strftime("%H:%M:%S"))
+    end
+  
+    it 'conditions the display of year appropriately' do
+      expect((w = Work.create!(args)).valid?).to eq(true)
+      expect(w.display_year).to eq((args[:year] + 2.days).year)
+    end
+
   end
 
   context 'object update' do
