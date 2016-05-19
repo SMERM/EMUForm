c082d0abe41ff89e1a99af78b0af1ab5717c0bca
Nicola Bernardini <nicb@sme-ccppd.org>
2016-05-19 02:19:18 +0200

    re-added missing javascript/stylesheet files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,21 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile +=
+  %w(
+      jquery/jquery.fileupload-angular.js
+      jquery/jquery.fileupload-audio.js
+      jquery/jquery.fileupload-image.js
+      jquery/jquery.fileupload-jquery-ui.js
+      jquery/jquery.fileupload.js
+      jquery/jquery.fileupload-process.js
+      jquery/jquery.fileupload-ui.js
+      jquery/jquery.fileupload-validate.js
+      jquery/jquery.fileupload-video.js
+      jquery/jquery.iframe-transport.js
+      jquery/vendor/jquery.ui.widget.js
+      jquery/jquery.fileupload.css
+      jquery/jquery.fileupload-noscript.css
+      jquery/jquery.fileupload-ui.css
+      jquery/jquery.fileupload-ui-noscript.css
+    )
